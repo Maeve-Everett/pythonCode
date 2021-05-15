ade02b4da53c6e705f0ad3970b4da18cd1852589
Kieran Everett <kieraneverett03@gmail.com>
2021-05-15 21:55:48 +0100

    Advent of code day 3 start

---
+++ adventofcode/2020/day-03/day-03.py
@@ -0,0 +1,17 @@
+f = open("adventofcode/2020/day-03/day-03-input.txt", "r")
+data = f.readlines()
+f.close()
+
+def part1():
+    
+    print(data)
+
+
+def part2():
+    
+    return 0
+
+
+if __name__ == "__main__":
+    print("Part 1: ", part1())
+    print("Part 2: ", part2())
