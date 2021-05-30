a82d76289284669dfc07df2fe3181a8fea6c5b45
Kieran Everett <kieraneverett03@gmail.com>
2021-05-30 16:03:34 +0100

    Update bf.py

--- bf/bf.py
+++ bf/bf.py
@@ -3,11 +3,14 @@ data_pointer = 0
 # instruction_pointer stores where the program is pointing to in the instructions
 instruction_pointer = 0
 # program stores the instructions of the program
-program = ""
+program = """++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."""
 # memory stores the memory of the program as an infititely long list of 1 byte numbers
 memory = [0]
+# output_memory saves what needs to be outputted
+output_memory = ""
 
 def increment_data_pointer():
+    global data_pointer, memory
     # If the data_pointer is at the end of currently used memory then it needs to create a new value in the list
     if data_pointer == len(memory)-1:
         memory.append(0)
@@ -16,11 +19,13 @@ def increment_data_pointer():
         data_pointer += 1
 
 def decrement_data_pointer():
+    global data_pointer
     # If the data_pointer is at the first index then it doesn't go further because the index starts at 0 and has an undefined length
     if data_pointer != 0:
         data_pointer -= 1
 
 def increment_data():
+    global memory
     # Increment the data at the current data_pointer by 1 and overflow if it goes above 255, has to be done manually because python doesn't have UINT8 variables
     if memory[data_pointer] == 255:
         memory[data_pointer] = 0
@@ -28,6 +33,7 @@ def increment_data():
         memory[data_pointer] += 1
 
 def decrement_data():
+    global memory
     # Decrement the data at the current data_pointer by 1 and overflow if it goes below 0, has to be doen manually because python doesn't have UINT8 variables
     if memory[data_pointer] == 0:
         memory[data_pointer] = 255
@@ -35,22 +41,34 @@ def decrement_data():
         memory[data_pointer] -= 1
 
 def output_character():
+    global output_memory
     # Prints the ascii representation of the current value at the data_pointer
-    character = chr(memory[data_pointer])
-    print(character)
+
+    if memory[data_pointer] == 10:
+        print(output_memory)
+        output_memory = ""
+    else:
+        character = chr(memory[data_pointer])
+        output_memory += character
 
 def input_character():
+    global memory, output_memory
     # Stores the ascii representation of the inputted value at the data_pointer
+    print(output_memory)
+    output_memory = ""
     character = ord(input())
     memory[data_pointer] = character
 
 def increment_instruction_pointer():
+    global instruction_pointer
     if instruction_pointer == len(program)-1:
-        print("End of File")
+        print("EOF")
+        quit()
     else:
         instruction_pointer += 1
 
 def decrement_instruction_pointer():
+    global instruction_pointer
     if instruction_pointer == 0:
         print("Start of File")
     else:
@@ -80,3 +98,25 @@ def jump_back():
 
 # the program needs to increment the pointer at the end of *everything* including jumping
 # i also need to do global stuff
+
+def main():
+    while True:
+        if program[instruction_pointer] == ">":
+            increment_data_pointer()
+        elif program[instruction_pointer] == "<":
+            decrement_data_pointer()
+        elif program[instruction_pointer] == "+":
+            increment_data()
+        elif program[instruction_pointer] == "-":
+            decrement_data()
+        elif program[instruction_pointer] == ".":
+            output_character()
+        elif program[instruction_pointer] == ",":
+            input_character()
+        elif program[instruction_pointer] == "[":
+            jump_forward()
+        elif program[instruction_pointer] == "]":
+            jump_back()
+        increment_instruction_pointer()
+
+main()
