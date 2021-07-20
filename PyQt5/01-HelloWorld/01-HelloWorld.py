9dbac87ef9043fb6391391df636ea8098c2b3d58
Kieran Everett <kieraneverett03@gmail.com>
2021-07-20 01:06:39 +0100

    Added a button

--- PyQt5/01-HelloWorld/01-HelloWorld.py
+++ PyQt5/01-HelloWorld/01-HelloWorld.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
 
+def clicky():
+    print("Button was clicked")
+
 app = QApplication(sys.argv)
 window = QMainWindow()
 window.setGeometry(400,400,300,300) # X, Y on screen, window Height, Width... why
@@ -10,6 +13,7 @@ button = QPushButton(window)
 button.setText("Hello World")
 #button.show() # Only needed if not using a QMainWindow
 button.move(100,100)
+button.clicked.connect(clicky)
 
 window.show()
 app.exec()
