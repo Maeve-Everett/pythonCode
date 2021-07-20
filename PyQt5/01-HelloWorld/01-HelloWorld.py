707f73dc7ba3de17699ba068a1da7dbb048ee22b
Kieran Everett <kieraneverett03@gmail.com>
2021-07-20 01:03:04 +0100

    Better Hello World

--- PyQt5/01-HelloWorld/01-HelloWorld.py
+++ PyQt5/01-HelloWorld/01-HelloWorld.py
@@ -1,7 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QPushButton
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
 
 app = QApplication(sys.argv)
-button = QPushButton("Hello World")
+window = QMainWindow()
+window.setGeometry(400,400,300,300) # X, Y on screen, window Height, Width... why
+window.setWindowTitle("Hello World")
+
+button = QPushButton(window)
+button.setText("Hello World")
 button.show()
+
+window.show()
 app.exec()
